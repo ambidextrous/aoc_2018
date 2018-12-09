64c2987fc30bb614575412e076bc1ce64e95a317
Aidan <butleraidan@gmail.com>
2018-12-09 14:28:57 +0100

    Linked list solution working with test example

--- 9a.go
+++ 9a.go
@@ -171,21 +171,34 @@ func insertMarbleAtIndex(marble int, index int, circle []int) []int {
 	return circle
 }
 
-func removeMarbleAtIndex(index int, circle []int) ([]int, int) {
-	retVal := circle[index]
+func removeMarbleAtIndex(index int, circle []int) []int {
 	if index == len(circle) {
 		circle = circle[:len(circle)-1]
-		return circle, retVal
+		return circle
 	}
 	firstPart := make([]int, 0)
 	firstPart = append(firstPart, circle[:index]...)
 	secondPart := make([]int, 0)
 	secondPart = append(secondPart, circle[index+1:]...)
 	circle = append(firstPart, secondPart...)
-	return circle, retVal
+	return circle
+}
+
+func getHighestPlayerScore(players map[int][]int) int {
+	highestScore := 0
+	for key := range players {
+		totalScore := sumSlice(players[key])
+		if totalScore > highestScore {
+			highestScore = totalScore
+		}
+	}
+	return highestScore
 }
 
 func playMarbleGame(numPlayers int, finalLastMarbleScore int) int {
+	REGULAR_JUMP_DIST := 2
+	SPECIAL_JUMP_DIST := 7
+	SPECIAL_DIVISOR := 23
 	circle := make([]int, 0)
 	players := make(map[int][]int)
 	for i := 1; i <= numPlayers; i++ {
@@ -196,21 +209,29 @@ func playMarbleGame(numPlayers int, finalLastMarbleScore int) int {
 	currentMarblePos := 0
 	nextMarblePos := 0
 	currentPlayer := 0
-	for currentMarbleScore < finalLastMarbleScore {
-		if roundCounter%23 == 0 && roundCounter != 0 {
+	//for currentMarbleScore < 27 {
+	for {
+		if roundCounter%SPECIAL_DIVISOR == 0 && roundCounter != 0 {
 			currentPlayer++
 			if currentPlayer > numPlayers {
 				currentPlayer = 1
 			}
-			removalPosition := currentMarblePos - 9
+			removalPosition := currentMarblePos - SPECIAL_JUMP_DIST - REGULAR_JUMP_DIST
 			if removalPosition < 0 {
-				removalPosition = len(circle) - removalPosition
+				removalPosition = len(circle) + removalPosition
 			}
-			nextMarblePos = currentMarblePos - 7
+			nextMarblePos = currentMarblePos - SPECIAL_JUMP_DIST
 			if nextMarblePos < 0 {
-				nextMarblePos = len(circle) - nextMarblePos
+				nextMarblePos = len(circle) + nextMarblePos
 			}
-			circle, _ = removeMarbleAtIndex(removalPosition, circle)
+			marbleScore := circle[removalPosition]
+			circle = removeMarbleAtIndex(removalPosition, circle)
+			playerRoundScore := marbleScore + roundCounter
+			players[currentPlayer] = append(players[currentPlayer], playerRoundScore)
+			if playerRoundScore == finalLastMarbleScore {
+				//fmt.Printf("lastMarbleWorth = %d \n", playerRoundScore)
+				return getHighestPlayerScore(players)
+			}
 		} else if roundCounter == 0 {
 			circle = insertMarbleAtIndex(roundCounter, currentMarblePos, circle)
 			nextMarblePos = 1
@@ -228,25 +249,129 @@ func playMarbleGame(numPlayers int, finalLastMarbleScore int) int {
 				currentPlayer = 1
 			}
 			circle = insertMarbleAtIndex(roundCounter, currentMarblePos, circle)
-			nextMarblePos = currentMarblePos + 2
+			nextMarblePos = currentMarblePos + REGULAR_JUMP_DIST
 			if nextMarblePos > len(circle) {
 				nextMarblePos = 1
 			}
 		}
-		fmt.Printf("round %d: player %d: %v\n", roundCounter, currentPlayer, circle)
+		//fmt.Println("currentMarblePos:")
+		//fmt.Println(currentMarblePos)
+		//fmt.Printf("round %d: player %d: %v\n", roundCounter, currentPlayer, circle)
 		roundCounter++
 		currentMarbleScore++
 		currentMarblePos = nextMarblePos
+		//fmt.Println()
+		if roundCounter%1000 == 0 {
+			fmt.Printf("roundCounter = %d\n", roundCounter)
+		}
 	}
-	return currentMarbleScore
+	return getHighestPlayerScore(players)
+}
+
+type node struct {
+	next     *node
+	nextName int
+	prev     *node
+	prevName int
+	name     int
+}
+
+func jumpBackDist(currentNode *node, dist int) (node, int) {
+	for i := 0; i < dist; i++ {
+		currentNode = currentNode.prev
+	}
+	replacementNode := node{name: currentNode.next.name, prev: currentNode.prev, prevName: currentNode.prevName, next: currentNode.next.next, nextName: currentNode.next.nextName}
+	replacementNode.next.prev = &replacementNode
+	replacementNode.next.prevName = replacementNode.name
+	replacementNode.prev.next = &replacementNode
+	replacementNode.prev.nextName = replacementNode.name
+	//fmt.Printf("!!!prevNode: %+v\n", replacementNode.prev)
+	//fmt.Printf("!!!nextNode: %+v\n", replacementNode.next)
+	return replacementNode, currentNode.name
+}
+
+func removeNode(currentNode *node) {
+	currentNode.prev.next = currentNode.next
+	currentNode.prev.nextName = currentNode.nextName
+	currentNode.next.prev = currentNode.prev
+	currentNode.next.prevName = currentNode.prevName
+	currentNode = currentNode.next
+}
+
+func playLinkedGame(numPlayers int, finalLastMarbleScore int) int {
+	players := make(map[int][]int)
+	for i := 1; i <= numPlayers; i++ {
+		players[i] = make([]int, 0)
+	}
+	// Set-up
+	zeroNode := node{name: 0}
+	oneNode := node{name: 1, next: &zeroNode, nextName: 0}
+	twoNode := node{name: 2, next: &oneNode, prev: &zeroNode, nextName: 1, prevName: 0}
+	zeroNode.prev = &oneNode
+	zeroNode.prevName = 1
+	zeroNode.next = &twoNode
+	zeroNode.nextName = 2
+	oneNode.prev = &twoNode
+	oneNode.prevName = 2
+	//fmt.Printf("%+v\n", zeroNode)
+	//fmt.Printf("%+v\n", oneNode)
+	//fmt.Printf("%+v\n", twoNode)
+	currentNode := twoNode
+	roundCounter := 3
+	playerCounter := 3
+	for roundCounter < finalLastMarbleScore {
+		if roundCounter%23 == 0 && roundCounter != 0 {
+			newScore := 0
+			currentNode, newScore = jumpBackDist(&currentNode, 7)
+			//fmt.Printf("!!!currentNode: %+v\n", currentNode)
+			//fmt.Printf("!!!newScore: %d\n", newScore)
+			//jumpedToNodeScore := currentNode.name
+
+			playerRoundScore := newScore + roundCounter
+			players[playerCounter] = append(players[playerCounter], playerRoundScore)
+			//if playerRoundScore == finalLastMarbleScore {
+			//	//fmt.Println("GOT HERE!")
+			//	return getHighestPlayerScore(players)
+			//}
+			removeNode(&currentNode)
+		} else {
+			followingNode := currentNode.next
+			newNode := node{name: roundCounter, next: followingNode.next, nextName: followingNode.nextName, prev: followingNode, prevName: followingNode.name}
+			newNode.next.prev = &newNode
+			newNode.next.prevName = newNode.name
+			followingNode.next = &newNode
+			followingNode.nextName = newNode.name
+			currentNode = newNode
+		}
+		//fmt.Printf("roundCounter: %d\n", roundCounter)
+		//fmt.Printf("playerCounter: %d\n", playerCounter)
+		//fmt.Printf("currentNode: %+v\n", currentNode)
+		//fmt.Println()
+		roundCounter++
+		playerCounter++
+		if playerCounter > numPlayers {
+			playerCounter = 1
+		}
+		if roundCounter%1000 == 0 {
+			fmt.Printf("roundCounter = %d\n", roundCounter)
+		}
+	}
+	return 0
 }
 
 func main() {
+	highestPlayerScore := playLinkedGame(9, 25)
+	//highestPlayerScore = playLinkedGame(10, 1618)
+	fmt.Println("highestPlayerScore:")
+	fmt.Println(highestPlayerScore)
 	//filename := "test"
 	//filename = "input8a"
 	//input := readFile(filename)
 	//input = strings.Replace(input, "\n", "", -1)
 	//fmt.Println("input:")
 	//fmt.Println(input)
-	playMarbleGame(9, 26)
+	//highestScore := playMarbleGame(17, 1104)
+	//highestScore := playMarbleGame(10, 1618)
+	//fmt.Println("highestScore:")
+	//fmt.Println(highestScore)
 }
